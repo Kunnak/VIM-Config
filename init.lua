09912af93e3b76da44bb862f95910461de204c6c
Yannick <132798545+Kunnak@users.noreply.github.com>
2025-07-13 21:54:19 +0200

    New changes for avoiding error notification

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Basiseinstellungen und Module laden
+vim.g.mapleader = " "
 require("Yannick.options")
 require("Yannick.keymaps")
 require("Yannick.plugins")
@@ -10,3 +11,12 @@ require("Yannick.lualine")
 require("Yannick.noice")
 require("Yannick.telescope")
 require("Yannick.signs")
+
+-- Setze die Highlight-Gruppe explizit, bevor notify geladen wird
+vim.cmd([[highlight NotifyBackground guibg=#000000]])
+
+require("notify").setup({
+  background_colour = "#000000",
+})
+
+vim.notify = require("notify")
